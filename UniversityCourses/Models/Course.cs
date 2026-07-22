5d45156e724cf2a32a004058a976ad7e3114d5b4
Rumbie09 <128155342+ST103339019@users.noreply.github.com>
2026-07-22 15:38:11 +0200

    Add: Course and Student models

---
+++ UniversityCourses/Models/Course.cs
@@ -0,0 +1,31 @@
+﻿using Azure;
+using Azure.Data.Tables;
+
+namespace UniversityCourses.Models
+{
+    
+    // This class represents one course stored in Azure Table Storage (Microsoft, 2026).
+
+    public class Course : ITableEntity
+    {
+        // Groups all course records together
+        public string PartitionKey { get; set; } = "Course";
+
+        // Creates a unique ID for every course
+        public string RowKey { get; set; } = Guid.NewGuid().ToString();
+
+        // Automatically updated by Azure
+        public DateTimeOffset? Timestamp { get; set; }
+
+        // Used by Azure for updates
+        public ETag ETag { get; set; }
+
+        // Course details
+        public string CourseCode { get; set; } = "";
+
+        public string CourseName { get; set; } = "";
+
+        public string Lecturer { get; set; } = "";
+    }
+}
+//Microsoft.2026. Azure.Data.Tables. [Online] https://learn.microsoft.com/en-us/dotnet/api/azure.data.tables?view=azure-dotnet.Accessed 22 July 2026.
---
+++ UniversityCourses/Models/Student.cs
@@ -0,0 +1,38 @@
+﻿using Azure;
+using Azure.Data.Tables;
+
+namespace UniversityCourses.Models
+{
+    // This class stores student information in Azure Table Storage (Microsoft, 2026).
+
+    public class Student : ITableEntity
+    {
+        // Groups all student records together
+        public string PartitionKey { get; set; } = "Student";
+
+        // Creates a unique ID for every student
+        public string RowKey { get; set; } = Guid.NewGuid().ToString();
+
+        public DateTimeOffset? Timestamp { get; set; }
+
+        public ETag ETag { get; set; }
+
+        // Student number assigned by the university
+        public string StudentNumber { get; set; } = "";
+
+        // Student's first name
+        public string FirstName { get; set; } = "";
+
+        // Student's last name
+        public string LastName { get; set; } = "";
+
+        // Student's email address
+        public string Email { get; set; } = "";
+
+        // Stores the course the student is enrolled in
+        public string EnrolledCourse { get; set; } = "";
+    }
+}
+
+// References
+// Microsoft 2026, Azure.Data.Tables, Microsoft Learn,[Online] Available at :https://learn.microsoft.com/en-us/dotnet/api/azure.data.tables?view=azure-dotnet. Accessed 22 July 2026.
